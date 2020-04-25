e00bb52e70810f6edca933ba151335130f02d279
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-04-25 06:08:08 -0500

    Added an Enum Class for the Directions (for Key Movements)

---
+++ include/mylibrary/direction.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 CS126SP20. All rights reserved.
+
+#ifndef FINALPROJECT_DIRECTION_H
+#define FINALPROJECT_DIRECTION_H
+
+namespace mylibrary {
+
+// Represents the possible directions of the snake.
+enum class Direction { kUp, kDown, kLeft, kRight };
+
+}  // namespace mylibrary
+
+#endif  // FINALPROJECT_DIRECTION_H_
