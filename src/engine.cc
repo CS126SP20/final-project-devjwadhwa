890e07a926a4e6f30d0a31460e5fa04d3e10afe9
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-04-25 06:22:13 -0500

    Added an Engine for Cursor Movement

---
+++ src/engine.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2020 [Dev Wadhwa]. All rights reserved.
+#include <stdexcept>
+
+#include <mylibrary/direction.h>
+#include <mylibrary/location.h>
+#include <mylibrary/engine.h>
+
+namespace mylibrary {
+
+// Converts a direction into a delta location.
+Location FromDirection(const Direction direction) {
+  switch (direction) {
+    case Direction::kUp:
+      return {-1, 0};
+    case Direction::kDown:
+      return {+1, 0};
+    case Direction::kLeft:
+      return {0, -1};
+    case Direction::kRight:
+      return {0, +1};
+  }
+
+  throw std::out_of_range("switch statement not matched");
+}
+
+Player Engine::GetPlayer() const {
+  return player_;
+}
+
+Engine::Engine(int width, int height)
+  : width_(width),
+    height_(height),
+    player_("White", 0, Location(0,0)){}
+
+void Engine::Step() {
+  Location d_loc = FromDirection(direction_);
+  Location new_head_loc =
+      (player_.GetLoc() + d_loc) % Location(height_, width_);
+
+  Location leader = new_head_loc;
+  player_.SetLoc(leader);
+
+  last_direction_ = direction_;
+}
+
+void Engine::SetDirection(const mylibrary::Direction direction) {
+  direction_ = direction;
+}
+
+
+
+
+}  // namespace mylibrary
