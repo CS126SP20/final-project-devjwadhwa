0efa9f954ca4f15025fdd263c80d4250f3eaeef3
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-04-27 21:36:08 -0500

    Added map class for map and screen reading

---
+++ include/mylibrary/map.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 [Dev Wadhwa]. All rights reserved
+
+#ifndef FINALPROJECT_MAP_H
+#define FINALPROJECT_MAP_H
+
+namespace mylibrary {
+
+class map {};
+
+}  // namespace mylibrary
+
+#endif  // FINALPROJECT_MAP_H
---
+++ src/map.cc
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 [Dev Wadhwa]. All rights reserved.
+
+#include "mylibrary/map.h"
+
+namespace mylibrary {
+
+Location::Location(int row, int col) : row_(row), col_(col) {}
+
+
+}  // namespace mylibrary
+
