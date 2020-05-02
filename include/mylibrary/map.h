b63e1537b5a8f575b38f09aad72e74bf99cad6ec
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-05-01 21:37:25 -0500

    Added default and explicit constructor for the map class

--- include/mylibrary/map.h
+++ include/mylibrary/map.h
@@ -19,17 +19,7 @@ class Map {
  public:
   Map();
   explicit Map(std::vector<std::vector<char>> game_screen);
-  Location GetPlayerNewLoc(const Map& curr_map, Engine engine);
-  std::string GetMapLabels();
-  void ReadImageLabels();
-  void ReadGameScreens();
-  void SetupMap(std::string map_line);
-  bool IsScreenChange();
-  int GetCurrScreenNum(const Map& curr_map);
-  int GetNewScreenNum();
-  int GetTransitionScreenNum(int num, char entry);
-  std::vector<Map> GetScreen();
-  enum EntryPoints { a = 97};
+
 
  private:
   int screen_num_{};
