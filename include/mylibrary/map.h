7cd86fb453dac005ef4b6e4e8a4f6505184f01c8
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-05-04 04:02:43 -0500

    Added comments and JAVADOC style comments in Map

--- include/mylibrary/map.h
+++ include/mylibrary/map.h
@@ -14,29 +14,130 @@ using mylibrary::Engine;
 
 class Map {
  public:
+  /**
+   * A cartesian system for the board
+   */
   char cartesian[16][16]{};
 
  public:
+  /**
+   * Constructor
+   */
   Map();
+
+  /**
+   * Explicit Constructor setting the cartesian to the game screen
+   *
+   * @param game_map : The current Game Map
+   */
   explicit Map(std::vector<std::vector<char>> game_map);
+
+  /**
+   * Reads all background images from the txt file
+   */
   void ReadBackgroundImages();
+
+  /**
+   * Reads all maps from the text file
+   */
   void ReadMaps();
+
+  /**
+   * Sets up map line by line
+   *
+   * @param map_line : One line of the map
+   */
   void SetupMap(std::string map_line);
+
+  /**
+   * Gets all the maps
+   *
+   * @return A vector containing all the maps
+   */
   std::vector<Map> GetMaps();
+
+  /**
+   * Gets the current map's key
+   *
+   * @param current_map : A map object of the current map
+   * @return The Key
+   */
   int GetCurrMapKey(const Map& current_map);
+
+  /**
+   * Gets the parallel map's key
+   *
+   * @param key : Current Map's Key
+   * @param door : Any exit point
+   *
+   * @return Parallel map's key
+   */
   int GetParallelMapKey(int key, char door);
+
+  /**
+   * Bool which tells if the screen is changing
+   *
+   * @return True if the screen changes
+   */
   bool IsScreenChange();
+
+  /**
+   * Gets the Parallel map's key
+   *
+   * @return The key
+   */
   int GetParallelMapNum();
+
+  /**
+   * Gets the Location of the Prisoner in the Parallel Map
+   *
+   * @param current_map : The current Map
+   * @param engine : The game engine
+   *
+   * @return The location of the Prisoner
+   */
   Location GetPlayerParallelLoc(const Map& current_map, Engine engine);
   std::string GetBackgroundKey();
 
-    private:
-  const int kBoardDimension = 16;
+ private:
+  /**
+   * Dimension of the board
+   */
+  const int kDimension = 16;
+
+  /**
+   * Constant for reallocating the Prisoner a position
+   */
+  const int kLoc = 2;
+
+  /**
+   * Screen number
+   */
   int screen_num_{};
+
+  /**
+   * Vector of strings saving every background image
+   */
   std::vector<std::string> background_images;
+
+  /**
+   * True if there is a change of screen
+   */
   bool is_screen_change_ = false;
+
+  /**
+   * Vector of Maps saving all the individual maps
+   */
   std::vector<mylibrary::Map> maze_maps;
+
+  /**
+   * Vector of char saving each map
+   */
   std::vector<std::vector<char>> map;
+
+  /**
+   * Exit and entry points
+   */
   std::string exits = "abcdef";
 };
 
--- src/map.cc
+++ src/map.cc
@@ -15,20 +15,17 @@ namespace mylibrary {
 Map::Map() = default;
 
 Map::Map(std::vector<std::vector<char>> game_map) {
-
   // A coordinate system
-  for (int i = 0; i < 16; i++) {
-    for (int j = 0; j < 16; j++) {
+  for (int i = 0; i < kDimension; i++) {
+    for (int j = 0; j < kDimension; j++) {
       this->cartesian[i][j] = game_map[i][j];
     }
   }
 }
 
 void Map::ReadBackgroundImages() {
-
   std::string background_file =
       "C:/Users/devjw/CLionProjects/cinder_0.9.2_vc2015/projects/FPPrisonBreak/assets/background.txt";
-
   std::ifstream images_file(background_file);
 
   while(!images_file.eof()) {
@@ -42,25 +39,25 @@ void Map::ReadBackgroundImages() {
 
 void Map::ReadMaps() {
   int maze_count = 0;
-
   std::string maze_file =
       "C:/Users/devjw/CLionProjects/cinder_0.9.2_vc2015/projects/FPPrisonBreak/assets/maze.txt";
-
   std::ifstream file(maze_file);
+
   while (!file.eof()) {
     std::string line_maze;
     std::getline(file, line_maze);
 
-    // Ignores empty lines
+    // Ignores empty lines, else setups Map
     if (!line_maze.empty()) {
       SetupMap(line_maze);
       maze_count++;
 
       // Ends one map
-      if (maze_count == kBoardDimension) {
+      if (maze_count == kDimension) {
         Map game_map = Map(map);
         maze_maps.push_back(game_map);
 
+        // Repeats the same
         maze_count = 0;
         map.clear();
       }
@@ -72,10 +69,10 @@ void Map::SetupMap(std::string map_line) {
   std::vector<char> map_char;
 
   // Reserves space for each line
-  map_char.reserve(kBoardDimension);
+  map_char.reserve(kDimension);
 
   // Pushes back each char
-  for (int i = 0; i < kBoardDimension; i++) {
+  for (int i = 0; i < kDimension; i++) {
     map_char.push_back(map_line.at(i));
   }
 
@@ -88,58 +85,47 @@ std::string Map::GetBackgroundKey() {
   for (int i = 0; i < background_images.size(); i++) {
     if (i == screen_num_) {
       std::cout<<background_images[i]<<std::endl;
-
-      //return map_labels_[i];
       key = background_images[i];
     }
   }
   return key;
 }
 
-std::vector<Map> Map::GetMaps() {
-  return maze_maps;
-}
-
 int Map::GetCurrMapKey(const Map& current_map) {
   int count = 0;
   for (int i = 0; i < maze_maps.size(); i++) {
-    for (int j = 0; j < kBoardDimension; j++) {
-      for (int k = 0; k < kBoardDimension; k++) {
+    for (int j = 0; j < kDimension; j++) {
+      for (int k = 0; k < kDimension; k++) {
 
         // Checking similarity block wise
         if (maze_maps[i].cartesian[j][k] == current_map.cartesian[j][k]) {
           count++;
 
+          // Checks if the current map is EXACTLY similar to the ith map
+          if (count == kDimension*kDimension) {
+            return i;
+          }
         } else {
           count = 0;
-          goto exitloop;
-        }
-
-        // Checks if the current map is EXACTLY similar to the ith map
-        if (count == kBoardDimension*kBoardDimension) {
-          return i;
         }
       }
     }
-    exitloop:;
   }
   return 0;
 }
 
 int Map::GetParallelMapKey(int key, char door) {
   for (int i = 0; i < maze_maps.size(); i++) {
-   // if (i != key) {
-      for (int j = 0; j < kBoardDimension; j++) {
-        for (int k = 0; k < kBoardDimension; k++) {
+    for (int j = 0; j < kDimension; j++) {
+      for (int k = 0; k < kDimension; k++) {
 
-          // Gets the map with the same door point as the current one
-          if (maze_maps[i].cartesian[j][k] == door && i != key) {
-            return i;
-          }
+        // Gets the map with the same door point as the current one
+        if (maze_maps[i].cartesian[j][k] == door && i != key) {
+          return i;
         }
       }
     }
-
+  }
   return 0;
 }
 
@@ -155,17 +141,22 @@ Location Map::GetPlayerParallelLoc(const Map& current_map, Engine engine) {
       is_screen_change_ = true;
 
       if (engine.GetDirection() == Direction::kUp) {
-        return {curr_col, kBoardDimension - 2};
+        return {curr_col, kDimension - kLoc};
       } else if (engine.GetDirection() == Direction::kDown) {
-        return {curr_col, 2};
+        return {curr_col, kLoc};
       } else if (engine.GetDirection() == Direction::kLeft) {
-        return {kBoardDimension - 2, curr_row};
+        return {kDimension - kLoc, curr_row};
       } else if (engine.GetDirection() == Direction::kRight){
-        return {1, curr_row};
+        return {kLoc - 1, curr_row};
       }
     }
   }
   return loc;
+}
+
+
+std::vector<Map> Map::GetMaps() {
+  return maze_maps;
 }
 
 bool Map::IsScreenChange() {
@@ -175,9 +166,6 @@ bool Map::IsScreenChange() {
 int Map::GetParallelMapNum() {
   return screen_num_;
 }
-
-
-
 
 }  // namespace mylibrary
 
